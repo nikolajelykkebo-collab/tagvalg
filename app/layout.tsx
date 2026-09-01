69bcfdf31e380b145f165a1e24e040e47082549c
Nikolaj <nikolaj.e.lykkebo@gmail.com>
2026-09-01 16:36:26 +0200

    Tilføjet Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 
 const poppins = Poppins({
@@ -22,7 +23,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="da">
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W8Z6DB33');`}
+        </Script>
+      </head>
       <body className={`${poppins.variable} min-h-screen antialiased`}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8Z6DB33"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
         {children}
       </body>
     </html>
